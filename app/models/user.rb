47341d3dee9a33bd7acd73a8d9628ffbe6f7919b
'deja douglas <'ddouglas@chicagotechacademy.org>
2014-04-22 13:21:37 -0500

    4/22/14

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 30}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.1]+\.[a-z]+\z/i
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
    uniqueness: { case_sensitive: false}
    validates :password, presence: true, length: { minimum: 5 }
    validates :password_confirmation, presence: true
