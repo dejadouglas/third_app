f8af4cc486f43b8ce50b49e059dd4165d3b9df9d
'deja douglas <'ddouglas@chicagotechacademy.org>
2014-04-28 12:58:43 -0500

    Part 18 (starting)

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+factory :user do
+name           "Deja"
+email          "ddouglas@chicagotechacademy.org"
+password       "fubar"
+password_confirmation "fubar"
+end	
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
 describe "UserPages" do
+
   subject { page }
   describe "signup page" do
    before { visit signup_path}
 
-   it{ should have_selector('h1', text: 'Sign up') }
+   it{ should have_selector('h1',    text: 'Sign up') }
    it{ should have_selector('title', text: 'Sign up') }
+
+  end
+
+  describe "profile page" do
+let(:user) { FactoryGirl.create(:user) }
+
+ before { visit signup_path}
+
+   it { should have_selector('h1',    text: user.name) }
+   it { should have_selector('title', text: user.name) }
 
   end
 end
